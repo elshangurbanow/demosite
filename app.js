f65acacb8127d53b39a77dbe5b93f75cb4d3226a
elshangurbanow <elsenqurbanov50@gmail.com>
2025-04-13 13:25:38 +0000

    fff

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(document).on("keypress", function(e) {
+        if (e.which == 13) {
+            let url = `https://apidemo-kappa.vercel.app/api/news`;
+            async function mynews() {
+                let responce = await fetch(url);
+                let data = await responce.json();
+                console.log(data);
+
+            }
+            mynews();
+
+        }
+    });
+});
